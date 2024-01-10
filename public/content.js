ac650cc60e601c2ca489942e9d77ddabffef7ebc
laura-jmr <laura.juergensmeier@gmail.com>
2024-01-10 11:26:06 +0100

    feat: section analysis

--- public/content.js
+++ public/content.js
@@ -1,24 +1,24 @@
 // // Filtert alle Bilder auf der Webseite heraus, extrahiert die Sources und lässt den Service-Worker einen Alt-Text
 // // generieren, der dann unter dem Bild angezeigt wird
-(async () => {
-    const images = document.querySelectorAll("img");
-    console.log(images);
+// (async () => {
+//     const images = document.querySelectorAll("img");
+//     console.log(images);
 
-    images.forEach(async img => {
-        const response = await chrome.runtime.sendMessage({ greeting: 'alt', imgUrl: img.dataset.src });
-        console.log(response.alt);
-        const altText = createDomElement(`
-            <p class="generated-alt-text">${response.alt}</p>
-        `);
-        img.parentNode.insertBefore(altText, img.nextSibling);
-    });
+//     images.forEach(async img => {
+//         const response = await chrome.runtime.sendMessage({ greeting: 'alt', imgUrl: img.dataset.src });
+//         console.log(response.alt);
+//         const altText = createDomElement(`
+//             <p class="generated-alt-text">${response.alt}</p>
+//         `);
+//         img.parentNode.insertBefore(altText, img.nextSibling);
+//     });
 
-    const manipuliert = createDomElement(`
-      <h1 class="test">HAHA ICH HAB EUCH MANIPULIERT</h1>
-    `);
+//     const manipuliert = createDomElement(`
+//       <h1 class="test">HAHA ICH HAB EUCH MANIPULIERT</h1>
+//     `);
 
-    document.body.append(manipuliert);
-})();
+//     document.body.append(manipuliert);
+// })();
 
 function createDomElement(html) {
     const dom = new DOMParser().parseFromString(html, 'text/html');
@@ -26,55 +26,90 @@ function createDomElement(html) {
 }
 
 (async () => {
-    // const mainDom = document.querySelector('main');
-    // console.log(mainDom);
+    const bodyDom = document.querySelector('body');
+    console.log(bodyDom);
 
-    // const elementsInMain = Array.from(mainDom.querySelectorAll('h1, h2, h3, p, span'));
-    // const result = extractTextContent(elementsInMain);
-    // console.log(result);
-    const pDoms = document.querySelectorAll('p');
-    console.log(pDoms);
+    const elementsInBody = Array.from(bodyDom.querySelectorAll('h1, h2, h3, p, span'));
+    const result = groupElementsBySection(elementsInBody);
+    console.log(result);
+    // const pDoms = document.querySelectorAll('p');
+    // console.log(pDoms);
 
-    pDoms.forEach(async paragraph => {
-        const response = await chrome.runtime.sendMessage({ greeting: 'leichteSprache', pText: paragraph.textContent });
+    // pDoms.forEach(async paragraph => {
+    //     const response = await chrome.runtime.sendMessage({ greeting: 'leichteSprache', pText: paragraph.textContent });
+    //     console.log(response);
+    //     console.log(response.leichteSprache);
+    //     const transformedResponse = response.leichteSprache.replace(/[.!?]/g, match => match + '<br><br>').trim();
+    //     console.log(transformedResponse);
+    //     paragraph.innerHTML = transformedResponse;
+    //     paragraph.classList.add("leichte-sprache");
+    // });
+
+    result.forEach(async section => {
+        const response = await chrome.runtime.sendMessage({ greeting: 'leichteSprache', section: section });
         console.log(response);
         console.log(response.leichteSprache);
-        const transformedResponse = response.leichteSprache.replace(/[.!?]/g, match => match + '<br><br>').trim().slice(1, -1);
-        console.log(transformedResponse);
-        paragraph.innerHTML = transformedResponse;
-        paragraph.classList.add("leichte-sprache");
+        const resArray = response.leichteSprache.elements;
+        resArray.forEach(domElement => {
+            domElement.innerHTML = domElement.innerText;
+            domElement.classList.add("leichte-sprache");
+        });
     });
 })();
 
 
 // Function to extract text content from an element
 function extractTextContent(elements) {
-    const sectionContents = [];
+    const sections = [];
     let currentSection = {};
 
     elements.forEach(element => {
         const tagName = element.tagName.toLowerCase();
         if (tagName === 'h1' || tagName === 'h2' || tagName === 'h3') {
-            // If new heading found, push current section content and start a new section
             if (Object.keys(currentSection).length !== 0) {
-                sectionContents.push(currentSection);
+                sections.push(currentSection);
             }
             currentSection = {
-                h1: tagName === 'h1' ? element.textContent.trim() : '',
-                h2: tagName === 'h2' ? element.textContent.trim() : '',
-                h3: tagName === 'h3' ? element.textContent.trim() : '',
-                p: ''
+                htmlElement: element,
+                content: {
+                    h1: tagName === 'h1' ? element.innerHTML.trim() : '',
+                    h2: tagName === 'h2' ? element.innerHTML.trim() : '',
+                    h3: tagName === 'h3' ? element.innerHTML.trim() : '',
+                    p: ''
+                }
             };
         } else if (tagName === 'p' || tagName === 'span') {
-            // Append text content to the current section's p attribute
-            currentSection.p += ' ' + element.textContent.trim();
+            currentSection.content.p += ' ' + element.innerHTML.trim();
+        }
+    });
+
+    if (Object.keys(currentSection).length !== 0) {
+        sections.push(currentSection);
+    }
+
+    return sections;
+}
+
+function groupElementsBySection(elements) {
+    const groupedSections = [];
+    let currentSection = { elements: [] };
+
+    elements.forEach(element => {
+        const tagName = element.tagName.toLowerCase();
+        if (tagName === 'h1' || tagName === 'h2' || tagName === 'h3' || tagName === 'p' || tagName === 'span') {
+            // If new heading or paragraph found, push current section and start a new one
+            if (currentSection.elements.length > 0 && (tagName == 'h1' || tagName == 'h2' || tagName == 'h3')) {
+                groupedSections.push(currentSection);
+                currentSection = { elements: [] };
+            }
+            currentSection.elements.push(element);
         }
     });
 
     // Push the last section if it exists
-    if (Object.keys(currentSection).length !== 0) {
-        sectionContents.push(currentSection);
+    if (currentSection.elements.length > 0) {
+        groupedSections.push(currentSection);
     }
 
-    return sectionContents;
+    return groupedSections;
 }
--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+/*global chrome*/
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import * as React from 'react';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
+
 
 function App() {
+  const [alternativeText, setAlternativeText] = React.useState(false);
+  const [easyLanguage, setEasyLanguage] = React.useState(false);
+
+  const handleAlternativeTextChange = async () => {
+    setAlternativeText((prev) => !prev);
+    // Send a message to background script when the alternativeText switch is toggled
+    const response = await chrome.runtime.sendMessage({ greeting: 'toggleAltText', toggleValue: alternativeText });
+        console.log(response.toggleAltText);
+  };
+
+  const handleEasyLanguageChange = async () => {
+    setEasyLanguage((prev) => !prev);
+    // Send a message to background script when the easyLanguage switch is toggled
+    const response = await chrome.runtime.sendMessage({ greeting: 'toggleEasyLanguage', toggleValue: easyLanguage });
+        console.log(response.toggleEasyLanguage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="popup-window">
+      <FormGroup id="popup-formgroup">
+        <FormControlLabel control={<Switch checked={alternativeText} onChange={handleAlternativeTextChange} />} label="Bild-Alternativtexte" />
+        <FormControlLabel control={<Switch checked={easyLanguage} onChange={handleEasyLanguageChange}/>} label="Leichte Sprache" />
+      </FormGroup>
+      <p id="popup-text"><span>An/Aus: </span>Command + B</p>
     </div>
   );
 }
